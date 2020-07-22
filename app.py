231ee4cdb9b34b5a9ea3222f3d45b59a491ae936
tchen00 <tammyc@mit.edu>
2020-07-22 13:58:02 -0400

    add database

--- app.py
+++ app.py
@@ -3,11 +3,20 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
+from flask_pymongo import PyMongo
+from flask import redirect
+from bson import ObjectId
 
 # -- Initialization section --
 app = Flask(__name__)
 
+# name of database
+app.config['MONGO_DBNAME'] = 'users'
+
+# URI of database
+app.config['MONGO_URI'] = "mongodb+srv://admin:WSXgwxWUj7lTtK0K@cluster0.slptq.mongodb.net/users?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
 
 # -- Routes section --
 @app.route('/')
